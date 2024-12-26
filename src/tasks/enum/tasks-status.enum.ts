a187ce9a75c23b81ee173bf73e2e9ceb3a4fe1ec
David <davidagamez04@gmail.com>
2024-12-26 08:19:10 -0500

    feat: add TaskState enum and list for task status management

---
+++ src/tasks/enum/tasks-status.enum.ts
@@ -0,0 +1,11 @@
+export enum TaskState {
+  TODO = 'todo',
+  IN_PROGRESS = 'in_progress',
+  COMPLETED = 'completed',
+}
+
+export const TaskStateList = [
+  TaskState.TODO,
+  TaskState.IN_PROGRESS,
+  TaskState.COMPLETED,
+];
