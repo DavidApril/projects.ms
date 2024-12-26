129c111b8507679da5bb8602b360d3d4988e7b37
David <davidagamez04@gmail.com>
2024-12-26 08:18:31 -0500

    feat: add CreateProjectDto for project creation validation

---
+++ src/projects/dto/create-project.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  owner: string;
+}
